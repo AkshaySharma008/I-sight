b5135c1fcdbdc9b9b573fd18ab9d235d62aeff8f
AkshaySharma008 <akshaysharma30121999@gmail.com>
2021-05-22 16:10:14 +0530

    Version 1

---
+++ reminder.js
@@ -0,0 +1,13 @@
+setTimeout(function() {
+    (function() {
+	document.body.innerHTML = `
+	<html>
+		<body>
+			<div id="alert-image">
+				<img src="${chrome.runtime.getURL("images/alert.png")}" id="image"/>
+			</div> 
+		</body>
+	</html>
+	`
+	})()
+}, 300);
